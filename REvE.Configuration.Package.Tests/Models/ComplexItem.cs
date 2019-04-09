cabdc3b2792a6f38043b2b71094c0b5164a00cbb
Ian Schlarman <ischlarman@gmail.com>
2019-04-09 14:01:33 -0500

    Updated to version 1.0.4; Added project to test packaged version.

---
+++ REvE.Configuration.Package.Tests/Models/ComplexItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace REvE.Configuration.Tests.Models
+{
+    [DataContract]
+    public class ComplexItem
+    {
+        [DataMember]
+        public List<ComplexItemFieldValue> ComplexFields { get; set; }
+    }
+
+    [Flags]
+    public enum ComplexItemFieldValue
+    {
+        None = 0,
+        One = 1 << 0,
+        Two = 1 << 1,
+        Three = One | Two
+    }
+}
---
+++ REvE.Configuration.Package.Tests/XmlConfigProviderTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel.Composition;
+
+namespace REvE.Configuration.Tests
+{
+    using Models;
+    [TestClass]
+    public class XmlConfigProviderTests : ConfigProviderTests<XmlConfigProvider<TestConfig>>
+    {
+        [Import("xml", typeof(IConfigProvider<>))]
+        protected override Lazy<XmlConfigProvider<TestConfig>> Configuration { get; set; }
+    }
+}
