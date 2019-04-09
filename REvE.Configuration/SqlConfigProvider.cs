f0483e01ed344950774f8e1a7c3ad1bf1a3654d2
Ian Schlarman <ischlarman@gmail.com>
2019-04-09 11:11:40 -0500

    Added REvE.Configuration solution and Project to source control.

---
+++ REvE.Configuration/SqlConfigProvider.cs
@@ -0,0 +1,42 @@
+﻿/*
+This source file is under MIT License (MIT)
+Copyright (c) 2019 Ian Schlarman
+https://opensource.org/licenses/MIT
+*/
+
+using System;
+
+namespace REvE.Configuration
+{
+    /// <summary>
+    /// Class for resolving a Configuration Model from a SQL Database.
+    /// </summary>
+    /// <typeparam name="TResult">The Configuration Model.</typeparam>
+    public class SqlConfigProvider<TResult> : ConfigProvider<TResult>
+    {
+        /// <summary>
+        /// Basic constructor for specifying the application configuration connection string name pointing to the database source.
+        /// </summary>
+        /// <param name="dataSourceKey">The connection string name in the Application Configuration.</param>
+        public SqlConfigProvider(string dataSourceKey) : base(dataSourceKey)
+        {
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        protected override void CloseConnection() => throw new NotImplementedException();
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <returns></returns>
+        protected override TResult Extract() => throw new NotImplementedException();
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="source"></param>
+        protected override void OpenConnection(string source) => throw new NotImplementedException();
+    }
+}
